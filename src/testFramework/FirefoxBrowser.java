56fe17d10e82ee680daf49f4c289e0fd4adc981a
Jiajun <jjwu.jiajun@gmail.com>
2015-01-26 23:23:48 -0800

    Implemented Firefox - loginTest

--- src/testFramework/FirefoxBrowser.java
+++ src/testFramework/FirefoxBrowser.java
@@ -1,39 +1,38 @@
 package testFramework;
 
-import java.util.concurrent.TimeUnit;
-import java.io.File;
-import java.io.IOException;
 import java.lang.System;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 
 public class FirefoxBrowser extends FirefoxDriver {
+	
+	public FirefoxBrowser(){//FirefoxProfile profile) {
+		//super(profile);
+		
+		// Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception
+		this.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
 	 
-	public FirefoxBrowser(FirefoxProfile profile) {
-		 super(profile);
-	 }
-	 
-public void loginTest() {
+	public void loginTest() {
         
-	// TODO Calculate loadtime
-    //Put a Implicit wait, this means that any search for elements on the page could take the time the implicit wait is set for before throwing exception
-    //this.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
- 
-    //Launch the Liquor.com Website
-    this.get("http://liquor.com");
- 
-    // Go through log in step 
-    this.findElement(By.className("ldc-user-login")).click();
-    this.findElement(By.id("login_username")).sendKeys("testuser_1"); 
-    this.findElement(By.id("login_password")).sendKeys("Test@123"); 
-    this.findElement(By.id("ldc-user-signin-button")).click();
- 
-    // Print a Log In message to the screen
-    System.out.println("Firefox login - Successful\n");
- 
-    // Find the element that's ID attribute is 'account_logout' (Log Out)
-    //this.findElement (By.xpath(".//*[@id='account_logout']/a")).click();
+		// TODO Calculate load time
+		
+	    //Launch the Liquor.com Website
+	    this.get("http://liquor.com");
+	 
+	    // Go through log in step 
+	    this.findElement(By.className("ldc-user-login")).click();
+	    this.findElement(By.id("login_username")).sendKeys("ghost5"); 
+	    this.findElement(By.id("login_password")).sendKeys("ghostghost"); 
+	    this.findElement(By.id("ldc-user-signin-button")).click();
+	 
+	    // Print a Log In message to the screen
+	    System.out.println("Firefox login - Successful\n");
+	 
+	    // Find the element that's ID attribute is 'account_logout' (Log Out)
+	    //this.findElement (By.xpath(".//*[@id='account_logout']/a")).click();
 	} 
 }
--- src/testFramework/main.java
+++ src/testFramework/main.java
@@ -1,9 +1,30 @@
 package testFramework;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.firefox.FirefoxProfile;
+
 public class main {
-
+	
 	public static void main(String[] args) {
-		System.out.println("hello world");
+	    // Firefox tests
+	    FirefoxBrowser firefox = new FirefoxBrowser();
+	    firefox.loginTest();
+	    //firefox.quit();
 	}
-
+	/*
+	public static FirefoxProfile getProfile() {
+		// Attaching blockSite extension to Firefox
+	    String blockSiteExtensionPath = "C:\\Users\\Jiajun\\workspace\\LDC\\extensions\\blocksite-1.1.8-fx.xpi";
+	    FirefoxProfile profile = new FirefoxProfile();
+	    
+	    try {
+	        profile.addExtension(new File(blockSiteExtensionPath));
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    
+	    return profile;
+	}*/
 }
